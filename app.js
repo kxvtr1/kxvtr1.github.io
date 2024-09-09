3ac536fb97ca69c4462b4b9b7fd2175b9429fa3c
kxvtr1 <karstuli21@gmail.com>
2024-09-09 15:49:15 +0100

    Website overhaul
    Community message board style website

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const images = document.querySelectorAll("img");
-    for (const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response=>response.json())
-        .then(data=> {
-            image.src=data.message 
-            image.width=100;
-            image.height=100;
+// document.addEventListener("DOMContentLoaded",()=>{
+//     const images = document.querySelectorAll("img");
+//     for (const image of images){
+//         fetch("https://dog.ceo/api/breeds/image/random")
+//         .then(response=>response.json())
+//         .then(data=> {
+//             image.src=data.message 
+//             image.width=300;
+//             image.height=300;
+//         })
+//     }
+// })
+
+document.addEventListener("DOMContentLoaded", () => {  
+    const imageContainer = document.getElementById("topboard"); 
+    const images = imageContainer.querySelectorAll("img");
+    
+    for (const image of images) {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+          image.src = data.message;
+          image.width = 300; 
+          image.height = 300; 
         })
+        .catch(error => {
+          console.error("Error fetching dog image:", error);        
+        });
     }
-})
+  });
+  
+  function fetchQuoteOfTheDay() {
+    const apiUrl = 'https://en.wikipedia.org/api/rest_v1/page/random/summary';
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const   
+   quote = data.extract;
+        document.getElementById('quote-container').textContent=quote;
+        console.log(quote); 
+        
+      })
+      .catch(error => {
+        console.error('Error fetching fact:', error);
+      });
+  }
+  
+  fetchQuoteOfTheDay();
+  
+  
